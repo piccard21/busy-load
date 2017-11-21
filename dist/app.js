3c6e3e94bab36fd0e93f31e8b562ee960560c48a
cafe-serendipity <cafe.serendipity21@gmail.com>
2017-11-21 13:28:25 +0100

    phantomjs kicked out

--- dist/app.js
+++ dist/app.js
@@ -26252,40 +26252,7 @@ jQuery = __webpack_require__(340);
     $.busyLoadFull = _busyLoad.busyLoadFull;
     $.fn.busyLoad.defaults = _defaults2.default;
 
-    // travis
-    // events
     // phantom-repbuit
-    // console
-
-    // $.busyLoadSetup({
-    //     animation: "slide",
-    //     background: "rgba(255, 152, 0, 0.86)"
-    // });
-    // let showHide = (event, tag, options = {}) => {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     $(tag).busyLoad("show", options);
-    //     $(tag).click(".busy-load-container", function() {
-    //         $(tag).busyLoad("hide");
-    //     });
-    // }
-    // // fullscreen
-    // $('.btn-fullscreen').click(function() {
-    //     $.busyLoadFull('show', {
-    //         background: "rgba(0, 51, 101, 0.83)",
-    //         image: "tardis",
-    //         animation: "slide"
-    //     });
-    //     $("body>.busy-load-container").click(function() {
-    //         $.busyLoadFull('hide', {
-    //             animation: "fade"
-    //         });
-    //     });
-    // })
-    // // spinners
-    // $(".spinner-01").click(function(e) {
-    //     showHide(e, "#spinner-01");
-    // });
 })(jQuery);
 
 /***/ }),
