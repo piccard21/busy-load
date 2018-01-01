46283025f5ca99683834ac25c11e1941b3f6e385
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-01-01 15:57:29 +0100

    tests adjusted

--- dist/app.js
+++ dist/app.js
@@ -497,41 +497,6 @@ jQuery = __webpack_require__(64);
     $.busyLoadSetup = _busyLoad.busyLoadSetup;
     $.busyLoadFull = _busyLoad.busyLoadFull;
     $.fn.busyLoad.defaults = _defaults2.default;
-
-    $.busyLoadSetup({
-        background: "rgba(0, 51, 101, 0.83)",
-        image: "http://cafe-serendipity.com/wp-content/uploads/2018/01/tardis.gif"
-    });
-    var showHide = function showHide(event, tag) {
-        var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-
-        event.preventDefault();
-        event.stopPropagation();
-        $(tag).busyLoad("show", options);
-        $(tag).click(".busy-load-container", function () {
-            $(tag).busyLoad("hide");
-        });
-    };
-    // fullscreen
-    $('.btn-fullscreen').click(function () {
-        $.busyLoadFull('show', {
-            animation: "slide"
-        });
-        $("body>.busy-load-container").click(function () {
-            $.busyLoadFull('hide', {
-                animation: "fade"
-            });
-        });
-    });
-    // spinners
-    $(".spinner-01").click(function (e) {
-        showHide(e, "#spinner-01");
-    });
-    $(".spinner-02").click(function (e) {
-        showHide(e, "#spinner-02", {
-            spinner: "accordion"
-        });
-    });
 })(jQuery);
 
 /***/ }),
