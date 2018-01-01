41634f9dd99150b1c66edd1e7fc74d6b2b65699c
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-01-01 15:17:14 +0100

    tardis removed

--- src/index.js
+++ src/index.js
@@ -13,40 +13,4 @@ jQuery = require('jquery');
     $.busyLoadSetup = busyLoadSetup;
     $.busyLoadFull = busyLoadFull;
     $.fn.busyLoad.defaults = defaults;
-
-
-    $.busyLoadSetup({
-        background: "rgba(0, 51, 101, 0.83)",
-        image: "http://cafe-serendipity.com/wp-content/uploads/2018/01/tardis.gif",
-    });
-    let showHide = (event, tag, options = {}) => {
-        event.preventDefault();
-        event.stopPropagation();
-        $(tag).busyLoad("show", options);
-        $(tag).click(".busy-load-container", function() {
-            $(tag).busyLoad("hide");
-        });
-    };
-    // fullscreen
-    $('.btn-fullscreen').click(function() {
-        $.busyLoadFull('show', {
-            animation: "slide"
-        });
-        $("body>.busy-load-container").click(function() {
-            $.busyLoadFull('hide', {
-                animation: "fade"
-            });
-        });
-    });
-    // spinners
-    $(".spinner-01").click(function(e) {
-        showHide(e, "#spinner-01");
-    });
-    $(".spinner-02").click(function(e) {
-        showHide(e, "#spinner-02", {
-            spinner: "accordion"
-        });
-    });
-
-
 }(jQuery));
