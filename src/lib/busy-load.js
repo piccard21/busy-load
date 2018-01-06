88121dffbea7160060b60163e1020d7b1f187fce
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-01-06 12:23:15 +0100

    multiple elements

--- src/lib/busy-load.js
+++ src/lib/busy-load.js
@@ -1,55 +1,56 @@
-import { BusyLoad } from './class.BusyLoad.js';
-import defaults  from './defaults.js';
+import {BusyLoad} from './class.BusyLoad.js';
+import defaults from './defaults.js';
 
 
-export function busyLoadSetup(settings) {  
+export function busyLoadSetup(settings) {
     $.extend(true, defaults, settings);
 }
 
 
-export function busyLoad(action, options) { 
-    let bl = new BusyLoad(this, JSON.parse(JSON.stringify(defaults)), options);
+export function busyLoad(action, options) {
+    
+    return this.each(function () {
+        let bl = new BusyLoad($(this), JSON.parse(JSON.stringify(defaults)), options);
 
-    switch (action) {
-        case "show":
-            bl.show();
-            break;
-        case "hide":
-            bl.hide();
-            break;
-        default:
-            throw `don't know action '${action}'`
-    }
-
-    return this;
+        switch (action) {
+            case "show":
+                bl.show();
+                break;
+            case "hide":
+                bl.hide();
+                break;
+            default:
+                throw `don't know action '${action}'`
+        }
+    });
 }
 
 
-export function busyLoadFull(action, options) {  
-    
+export function busyLoadFull(action, options) {
+
     let $body = $('body');
     let bl = new BusyLoad($body, JSON.parse(JSON.stringify(defaults)), options);
 
- 
+
     switch (action.toLowerCase()) {
         case "show":
-            $body.addClass("no-scroll"); 
+            $body.addClass("no-scroll");
             bl.caller = $body;
             bl.extendSettings({
                 fullScreen: true
             });
             bl.show();
- 
+
             break;
-            
+
         case "hide":
             bl.hide();
-            $body.removeClass("no-scroll"); 
+            $body.removeClass("no-scroll");
             break;
     }
 
     return $body;
- 
+
 }
 
  
